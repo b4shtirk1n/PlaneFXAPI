21d428ccaad805cb3d3a4adfb79cc612485538ca
b4shtirk1n <wowan4512@gmail.com>
2024-09-10 09:51:45 +0300

    add attr

--- PlaneFX/Controllers/SubscribeController.cs
+++ PlaneFX/Controllers/SubscribeController.cs
@@ -4,6 +4,9 @@ using PlaneFX.Services;
 
 namespace PlaneFX.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
     public class SubscribeController(SubscribeService subscribeService) : ControllerBase
     {
         [HttpGet]
