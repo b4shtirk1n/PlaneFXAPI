a075bb6afe0f36e2b97d291e62b9debbd0ce3d07
b4shtirk1n <wowan4512@gmail.com>
2024-09-11 14:45:30 +0300

    add create invoice method

--- PlaneFX/Services/SubscribeService.cs
+++ PlaneFX/Services/SubscribeService.cs
@@ -1,11 +1,27 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using PlaneFX.Models;
 
 namespace PlaneFX.Services
 {
-    public class SubscribeService(PlaneFXContext context)
+    public class SubscribeService(IConfiguration configuration, PlaneFXContext context)
     {
         public async Task<IEnumerable<Subscription>> Get()
             => await context.Subscriptions.ToListAsync();
+
+        public async Task<string> CreateInvoice(Invoice invoice)
+        {
+            using HttpClient client = new();
+            string json = JsonSerializer.Serialize(invoice);
+            var content = new StringContent(JsonSerializer.Serialize(invoice),
+                Encoding.UTF8, "application/json");
+
+            string token = configuration[StartupService.TOKEN]!;
+            var response = await client.PostAsync($"https://api.telegram.org/{token}/createInvoiceLink",
+                content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
     }
 }
