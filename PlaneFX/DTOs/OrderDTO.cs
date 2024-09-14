839898e412ed3cae035f934ff3a4b12688e3d140
b4shtirk1n <wowan4512@gmail.com>
2024-09-14 11:01:50 +0300

    update dto

--- PlaneFX/DTOs/OrderDTO.cs
+++ PlaneFX/DTOs/OrderDTO.cs
@@ -11,6 +11,27 @@ namespace PlaneFX.DTOs
         public required string AccountOwner { get; set; }
 
         [Required]
+        public required decimal Profit { get; set; }
+
+        [Required]
+        public required decimal ProfitToday { get; set; }
+
+        [Required]
+        public required decimal ProfitYesterday { get; set; }
+
+        [Required]
+        public required decimal ProfitWeek { get; set; }
+
+        [Required]
+        public required decimal MarginLevel { get; set; }
+
+        [Required]
+        public required decimal Drawdown { get; set; }
+
+        [Required]
+        public required decimal Balance { get; set; }
+
+        [Required]
         public required List<OpenedOrderDTO> OpenedOrders { get; set; }
 
         public List<ClosedOrderDTO> ClosedOrders { get; set; } = [];
