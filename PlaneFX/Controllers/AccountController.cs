0b12a035493a6a91864b1bf352d8895e5ecb22c7
b4shtirk1n <wowan4512@gmail.com>
2024-09-15 15:26:46 +0300

    update return type

--- PlaneFX/Controllers/AccountController.cs
+++ PlaneFX/Controllers/AccountController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PlaneFX.DTOs;
-using PlaneFX.Models;
+using PlaneFX.Responses;
 using PlaneFX.Services;
 
 namespace PlaneFX.Controllers
@@ -11,17 +11,17 @@ namespace PlaneFX.Controllers
 	public class AccountController(AccountService accountService) : ControllerBase
 	{
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Account>> GetById(long id)
-			=> await accountService.GetById(id) is Account account
+		public async Task<ActionResult<AccountResponse>> GetById(long id)
+			=> await accountService.GetById(id) is AccountResponse account
 				? Ok(account)
 				: NotFound();
 
 		[HttpGet("User/{id}")]
-		public async Task<ActionResult<IEnumerable<Account>>> GetByUser(long id)
+		public async Task<ActionResult<IEnumerable<AccountResponse>>> GetByUser(long id)
 			=> Ok(await accountService.GetByUser(id));
 
 		[HttpPost]
-		public async Task<ActionResult<Account>> Create(AccountDTO dTO)
+		public async Task<ActionResult<AccountResponse>> Create(AccountDTO dTO)
 		  	=> await accountService.IsExist(dTO)
 				? Ok(await accountService.Create(dTO))
 				: Conflict();
