dba24aa40939c9724d9e85a87e02f6d0dfae9430
b4shtirk1n <wowan4512@gmail.com>
2024-09-20 23:06:52 +0300

    add create & complete methods

--- PlaneFX/Services/CommandService.cs
+++ PlaneFX/Services/CommandService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PlaneFX.DTOs;
 using PlaneFX.Models;
 
 namespace PlaneFX.Services
@@ -7,5 +8,24 @@ namespace PlaneFX.Services
     {
         public async Task<IEnumerable<Command>> GetUnComplete()
             => await context.Commands.Where(c => !c.IsComplete).ToListAsync();
+
+        public async Task Create(CommandDTO dTO)
+        {
+            await context.Commands.AddAsync(new Command
+            {
+                Account = dTO.Account,
+                Order = dTO.Order,
+                Type = dTO.Type,
+                Ticker = dTO.Ticker,
+            });
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Complete(long id)
+        {
+            var command = await context.Commands.FindAsync(id);
+            command!.IsComplete = true;
+            await context.SaveChangesAsync();
+        }
     }
 }
