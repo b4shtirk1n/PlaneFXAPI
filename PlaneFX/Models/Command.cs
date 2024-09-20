c613b285b0d6ba521b1fdddc8ae84fe3001e9b4e
b4shtirk1n <wowan4512@gmail.com>
2024-09-20 22:05:36 +0300

    create model

---
+++ PlaneFX/Models/Command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlaneFX.Models;
+
+[Table("command")]
+public partial class Command
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Column("account")]
+    public long Account { get; set; }
+
+    [Column("order")]
+    public long? Order { get; set; }
+
+    [Column("tiker")]
+    public int Tiker { get; set; }
+
+    [Column("type")]
+    public int Type { get; set; }
+
+    [Column("is_complete")]
+    public bool IsComplete { get; set; }
+
+    [ForeignKey("Account")]
+    [InverseProperty("Commands")]
+    public virtual Account AccountNavigation { get; set; } = null!;
+
+    [ForeignKey("Type")]
+    [InverseProperty("Commands")]
+    public virtual CommandType TypeNavigation { get; set; } = null!;
+}
---
+++ PlaneFX/Models/CommandType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlaneFX.Models;
+
+[Table("command_type")]
+public partial class CommandType
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name", TypeName = "character varying")]
+    public string Name { get; set; } = null!;
+
+    [InverseProperty("TypeNavigation")]
+    public virtual ICollection<Command> Commands { get; set; } = new List<Command>();
+}
