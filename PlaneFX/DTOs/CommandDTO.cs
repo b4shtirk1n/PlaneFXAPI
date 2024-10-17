45501a7e8a03dd31023cf52b7dc39de39678babc
b4shtirk1n <wowan4512@gmail.com>
2024-10-17 13:08:26 +0500

    add order type

--- PlaneFX/DTOs/CommandDTO.cs
+++ PlaneFX/DTOs/CommandDTO.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace PlaneFX.DTOs
 {
@@ -9,10 +10,16 @@ namespace PlaneFX.DTOs
 
         public long? Order { get; set; }
 
+        [Precision(10, 5)]
+        public decimal Volume { get; set; }
+
         [StringLength(6)]
         public string? Ticker { get; set; }
 
         [Required]
         public required int Type { get; set; }
+
+        [StringLength(15)]
+        public required string OrderType { get; set; }
     }
 }
