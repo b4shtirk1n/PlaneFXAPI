ddcb4d385b4e846350634dc762034bd8d5490196
b4shtirk1n <wowan4512@gmail.com>
2024-10-29 23:21:59 +0500

    FIX: remove top level statements

--- PlaneFX/Program.cs
+++ PlaneFX/Program.cs
@@ -7,44 +7,50 @@ using PlaneFX.Middlewares;
 using PlaneFX.Models;
 using PlaneFX.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers().AddJsonOptions(o =>
+namespace PlaneFX
 {
-	o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-	o.JsonSerializerOptions.WriteIndented = true;
-});
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddDbContext<PlaneFXContext>(ServiceLifetime.Transient);
-
-builder.Services.AddTransient<DbConnectionMiddleware>();
-
-builder.Services.AddTransient<AuthFilter>();
-builder.Services.AddTransient<AdminFilter>();
-
-builder.Services.AddTransient<StartupService>();
-builder.Services.AddTransient<AppService>();
-builder.Services.AddTransient<AdminService>();
-builder.Services.AddTransient<UserService>();
-builder.Services.AddTransient<AccountService>();
-builder.Services.AddTransient<OrderService>();
-builder.Services.AddTransient<SubscribeService>();
-builder.Services.AddTransient<CommandService>();
-
-builder.Services.AddSwaggerGen(o =>
-{
-	o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+	public class Program
 	{
-		Name = nameof(Authorization),
-		Type = SecuritySchemeType.ApiKey,
-		Scheme = JwtBearerDefaults.AuthenticationScheme,
-		BearerFormat = nameof(System.IdentityModel.Tokens.Jwt).ToUpper(),
-		In = ParameterLocation.Header,
-		Description = "JSON Web Token based security (\"Bearer {Token}\")",
-	});
-	o.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
+		private static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllers().AddJsonOptions(o =>
+			{
+				o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+				o.JsonSerializerOptions.WriteIndented = true;
+			});
+			builder.Services.AddEndpointsApiExplorer();
+
+			builder.Services.AddDbContext<PlaneFXContext>(ServiceLifetime.Transient);
+
+			builder.Services.AddTransient<DbConnectionMiddleware>();
+
+			builder.Services.AddTransient<AuthFilter>();
+			builder.Services.AddTransient<AdminFilter>();
+
+			builder.Services.AddTransient<StartupService>();
+			builder.Services.AddTransient<AppService>();
+			builder.Services.AddTransient<AdminService>();
+			builder.Services.AddTransient<UserService>();
+			builder.Services.AddTransient<AccountService>();
+			builder.Services.AddTransient<OrderService>();
+			builder.Services.AddTransient<SubscribeService>();
+			builder.Services.AddTransient<CommandService>();
+
+			builder.Services.AddSwaggerGen(o =>
+			{
+				o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+				{
+					Name = nameof(Authorization),
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = JwtBearerDefaults.AuthenticationScheme,
+					BearerFormat = nameof(System.IdentityModel.Tokens.Jwt).ToUpper(),
+					In = ParameterLocation.Header,
+					Description = "JSON Web Token based security (\"Bearer {Token}\")",
+				});
+				o.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
 		{
 			new OpenApiSecurityScheme
 			{
@@ -56,34 +62,37 @@ builder.Services.AddSwaggerGen(o =>
 			},
 			Array.Empty<string>()
 		}
-	});
-});
+				});
+			});
 
-var app = builder.Build();
+			var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
-if (app.Environment.IsDevelopment())
-{
-	app.UseCors(o =>
-	{
-		o.AllowAnyOrigin();
-		o.AllowAnyHeader();
-		o.AllowAnyMethod();
-	});
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseCors(o =>
+				{
+					o.AllowAnyOrigin();
+					o.AllowAnyHeader();
+					o.AllowAnyMethod();
+				});
+			}
+			else
+			{
+				app.UseCors(o =>
+				{
+					o.WithOrigins("https://planefx.cloud/", "https://www.planefx.cloud/");
+					o.AllowAnyHeader();
+					o.AllowAnyMethod();
+				});
+			}
+			app.UseMiddleware<DbConnectionMiddleware>();
+			app.MapControllers();
+			app.Run();
+
+			await app.Services.GetRequiredService<StartupService>().MakeSU();
+		}
+	}
 }
-else
-{
-	app.UseCors(o =>
-	{
-		o.WithOrigins("https://planefx.cloud/", "https://www.planefx.cloud/");
-		o.AllowAnyHeader();
-		o.AllowAnyMethod();
-	});
-}
-app.UseMiddleware<DbConnectionMiddleware>();
-app.MapControllers();
-app.Run();
-
-await app.Services.GetRequiredService<StartupService>().MakeSU();
