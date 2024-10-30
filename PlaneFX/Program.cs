6194184c8f8eaeac34d42ee9d9464997f3f5965b
b4shtirk1n <wowan4512@gmail.com>
2024-10-30 20:48:50 +0500

    override Microsoft log level to warn & asp net to info

--- PlaneFX/Program.cs
+++ PlaneFX/Program.cs
@@ -19,8 +19,8 @@ namespace PlaneFX
 
 			builder.Services.AddSerilog((s, l) => l
 				.MinimumLevel.Information()
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-				.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
 				.WriteTo.Console());
 
 			builder.Services.AddControllers().AddJsonOptions(o =>
